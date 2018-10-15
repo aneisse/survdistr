5fa26963f75d611e2d3f0389a83cbaf33fb1598c
aneisse <a.neisse@gmail.com>
2018-10-15 13:36:15 -0300

    Developer update

--- R/kumgg.R
+++ R/kumgg.R
@@ -2,8 +2,84 @@
 # Kumaraswamy Generalized Gamma
 #==========================================================
 
+#' Kumaraswamy Generalized Gamma Distribution
+#'
+#' Density, distribution function, quantile function and random generation
+#' for the KumGG distribution with parameters \code{lambda}, \code{phi},
+#' \code{tau}, \code{alpha} and \code{k}.
+#'
+#' The KumGG distribution was described by Ortega \emph{et al} (2011) and has density
+#'
+#' \deqn{f(x) = (\lambda\phi\tau)/(\alpha\Gamma[k])(x/\alpha)^(\tauk-1)e^(-(x/\alpha)^\tau)
+#' (\gamma[k, (x/\alpha)^\tau])^(\lambda-1)(1-(\gamma[k, (x/\alpha)^\tau])^\lambda)^(\phi-1)}
+#'
+#' where \eqn{\gamma[., .]} is the incomplete gamma ratio and \eqn{\Gamma[.]} is the gamma funcion.
+#' The scale parameter is \eqn{\alpha}, the shape parameters are \eqn{\lambda}, \eqn{\phi} and
+#' \eqn{\tau} and \eqn{k}. The parameters \eqn{\lambda} and \eqn{phi}, come from the Kumaraswamy
+#' Generalized family introduced by Cordeiro and Castro (2011).
+#'
+#' With \code{phi = 1} KumGG becomes the Exponentiated Generalized Gamma distribution described
+#' by Cordeiro \emph{et al} (2011). Additionally, if \code{tau = k = 1} the Exponentiated Exponential.
+#'
+#' When \code{k = 1} then the KumGG distribution becomes the KumW distribution
+#' described by Cordeiro \emph{et al} (2010). For \code{k = 1} the KumGG becomes KumG distribution.
+#'
+#' The above are arguably the most important sub-models of KumGG. More sub-models are described in
+#' Ortega \emph{et al} (2011).
+#'
+#' @return \code{dkumgg} gives the density, \code{pkumgg} gives the distribution
+#' function, \code{qkumgg} gives the quantile function, and \code{rkumgg} generates random values.
+#'
+#' The length of the result is determined by \code{n} for \code{rkumgg}, for the other fucntions the
+#' length is the same as the vector passed to the first argument.
+#'
+#' Only the first element of the logical arguments are used.
+#'
+#' @source The source code of all distributions in this package can also be
+#' found on the \href{https://github.com/aneisse/survdistr}{survdistr} Github repository.
+#'
+#' @author Anderson Neisse <a.neisse@@gmail.com>
+#'
+#' @references
+#' ORTEGA, E. M. M.; CORDEIRO, G. M.; PASCOA, M. A. R. The generalized gamma
+#' geometric distribution. Journal of Statistical Theory and Applications,
+#' 2011, 10.3: 433-454.
+#'
+#' CORDEIRO, G. M.; ORTEGA, E. M. M; SILVA, G. O. The exponentiated generalized
+#' gamma distribution with application to lifetime data. Journal of statistical
+#' computation and simulation, 2011, 81.7: 827-842.
+#'
+#' CORDEIRO, G. M.; ORTEGA, E. M. M; NADARAJAH, S.. The Kumaraswamy Weibull
+#' distribution with application to failure data. Journal of the Franklin
+#' Institute, 2010, 347.8: 1399-1429.
+#'
+#' CORDEIRO, G. M.; DE CASTRO, M. A new family of generalized distributions. Journal of
+#' statistical computation and simulation, 2011, 81.7: 883-898.
+#'
+#' @seealso LINK TO OTHER PACKAGE DISTRIBUTIONS
+#'
+#' @examples
+#' # Generating values and comparing with the function
+#' x <- rkumgg(10000, tau = 0.9, alpha = 2, k = 0.5, lambda = 3, phi = 10)
+#' hist(x, probability = T, breaks = 100)
+#' curve(dkumgg(x, tau = 0.9, alpha = 2, k = 0.5, lambda = 3, phi = 10),
+#'       from = 0, to = 3, add = T)
+#'
+#' @param x,q numeric vector of quantiles.
+#' @param lambda shape parameter \eqn{\lambda > 0}.
+#' @param phi shape parameter \eqn{\phi \ge 0}.
+#' @param alpha scale parameter \eqn{\alpha > 0}.
+#' @param gamma shape parameter \eqn{\gamma > 0}.
+#' @param n desired size of the random number sample.
+#' @param lower.tail logical; if \code{TRUE}, probabilities are \eqn{P[X \le x]}, otherwise, \eqn{P[X \ge x]}
+#' @param log,log.p logical; if \code{TRUE}, probabilities/densities \code{p} are given as \code{log(p)}.
+#' @param cens.prop proportion of censored data to be simulated. If greater than \code{0}, a matrix will be
+#' returned instead of a vector. The matrix will contain the random values and a censorship indicator variable.
+#' @name KumGG
+NULL
 
 # Density KumGG
+#' @rdname KumGG
 dkumgg<-function(x, tau, alpha=1, k, lambda, phi, log = FALSE)
 {
   gr <- pgengamma.orig(q = x, shape = tau, scale = alpha, k = k)
@@ -14,6 +90,7 @@ dkumgg<-function(x, tau, alpha=1, k, lambda, phi, log = FALSE)
 }
 
 # Probability KumGG
+#' @rdname KumGG
 pkumgg<-function(q, tau, alpha=1, k, lambda, phi, lower.tail=TRUE, log.p = FALSE)
 {
   gr <- pgengamma.orig(q = q, shape = tau, scale = alpha, k = k)
@@ -25,6 +102,7 @@ pkumgg<-function(q, tau, alpha=1, k, lambda, phi, lower.tail=TRUE, log.p = FALSE
 }
 
 # Quantile KumGG
+#' @rdname KumGG
 qkumgg<-function(p, tau, alpha=1, k, lambda, phi, lower.tail=TRUE, log.p = FALSE)
 {
   u <- (1-(1-p)^(1/phi))^(1/lambda)
@@ -34,6 +112,7 @@ qkumgg<-function(p, tau, alpha=1, k, lambda, phi, lower.tail=TRUE, log.p = FALSE
 }
 
 # Hazard KumGG
+#' @rdname KumGG
 hkumgg<-function(q, tau, alpha=1, k, lambda, phi)
 {
   #d<-dkumgg(x = q, tau = tau, alpha = alpha, k = k, lambda = lambda, phi = phi)
@@ -45,6 +124,7 @@ hkumgg<-function(q, tau, alpha=1, k, lambda, phi)
 }
 
 # Cumulative Hazard KumGG
+#' @rdname KumGG
 Hkumgg<-function(q, tau, alpha=1, k, lambda, phi)
 {
   p<-pkumgg(q = q, tau = tau, alpha = alpha, k = k, lambda = lambda, phi = phi, lower.tail=FALSE)
@@ -52,6 +132,7 @@ Hkumgg<-function(q, tau, alpha=1, k, lambda, phi)
 }
 
 # Random KumGG
+#' @rdname KumGG
 rkumgg<-function(n, tau, alpha=1, k, lambda, phi,cens.prop=0)
 {
   if(cens.prop<0 | cens.prop>1){stop("cens.prop must be a numeric between 0 and 1.")}
@@ -71,6 +152,7 @@ rkumgg<-function(n, tau, alpha=1, k, lambda, phi,cens.prop=0)
 }
 
 # Maximum Likelihood Parameter estimation KumGG
+#' @rdname KumGG
 ml.kumgg<-function(x, tau.ini, alpha.ini, k.ini, lambda.ini, phi.ini)
 {
   l.kumgg<-function(param)
--- R/kumpar.R
+++ R/kumpar.R
@@ -1,5 +1,71 @@
-# Kumarasuammy Pareto ---------------------------
+#==========================================================
+# Kumarasuammy Pareto
+#==========================================================
+
+#' Kumarasuammy Pareto Distribution
+#'
+#' Density, distribution function, quantile function and random generation
+#' for the KumPar distribution with parameters \code{lambda}, \code{phi},
+#' \code{beta} and \code{k}.
+#'
+#' The KumLL distribution was described by Pereira \emph{et al} (2012) and has density
+#'
+#' \deqn{f(x) = (\lambda\phik\beta^k)/(x^(k+1))(1-(\beta/x)^k)^(\lambda-1)
+#' (1-(1-(\beta/x)^k)^\lambda)^(\phi-1)}
+#' for \eqn{x > \beta} and with scale parameter \eqn{\beta}, shape parameters \eqn{\lambda}, \eqn{\phi} and
+#' \eqn{k}.
+#'
+#' The parameters \eqn{\lambda} and \eqn{phi}, come from the Kumaraswamy
+#' Generalized family introduced by Cordeiro and Castro (2011).
+#'
+#' With \code{phi = 1} KumPar becomes the Exponentiated Pareto distribution.
+#' In addition, when \code{lambda = 1} it becomes the Pareto distribution.
+#'
+#' @return \code{dkumpar} gives the density, \code{pkumpar} gives the distribution
+#' function, \code{qkumpar} gives the quantile function, and \code{rkumpar} generates random values.
+#'
+#' The length of the result is determined by \code{n} for \code{rkumpar}, for the other fucntions the
+#' length is the same as the vector passed to the first argument.
+#'
+#' Only the first element of the logical arguments are used.
+#'
+#' @source The source code of all distributions in this package can also be
+#' found on the \href{https://github.com/aneisse/survdistr}{survdistr} Github repository.
+#'
+#' @author Anderson Neisse <a.neisse@@gmail.com>
+#'
+#' @references
+#' PEREIRA, M. B.; SILVA, R. B.; ZEA, L. M.; CORDEIRO, G. M. The kumaraswamy Pareto
+#' distribution. arXiv preprint arXiv:1204.1389, 2012.
+#'
+#' CORDEIRO, G. M.; DE CASTRO, M. A new family of generalized distributions. Journal of
+#' statistical computation and simulation, 2011, 81.7: 883-898.
+#'
+#' @seealso LINK TO OTHER PACKAGE DISTRIBUTIONS
+#'
+#' @examples
+#' # Generating values and comparing with the function
+#' x <- rkumpar(10000, beta = 2, k = 0.5, lambda = 3, phi = 10)
+#' hist(x, probability = T, breaks = 100)
+#' curve(dkumpar(x, beta = 2, k = 0.5, lambda = 3, phi = 10),
+#'       from = 2, to = 80, add = T)
+#'
+#' @param x,q numeric vector of quantiles \eqn{x > \beta}.
+#' @param lambda shape parameter \eqn{\lambda > 0}.
+#' @param phi shape parameter \eqn{\phi \ge 0}.
+#' @param beta scale parameter \eqn{\beta > 0}.
+#' @param k shape parameter \eqn{\k > 0}.
+#' @param n desired size of the random number sample.
+#' @param lower.tail logical; if \code{TRUE}, probabilities are \eqn{P[X \le x]}, otherwise, \eqn{P[X \ge x]}
+#' @param log,log.p logical; if \code{TRUE}, probabilities/densities \code{p} are given as \code{log(p)}.
+#' @param cens.prop proportion of censored data to be simulated. If greater than \code{0}, a matrix will be
+#' returned instead of a vector. The matrix will contain the random values and a censorship indicator variable.
+#' @name KumPar
+NULL
+
+
 # Density KumPar
+#' @rdname KumPar
 dkumpar<-function(x, beta, k, lambda, phi, log = FALSE)
 {
   dens <- (lambda*phi*k*beta^k)/(x^(k+1))*(1-(beta/x)^k)^(lambda-1)*(1-(1-(beta/x)^k)^lambda)^(phi-1)
@@ -9,6 +75,7 @@ dkumpar<-function(x, beta, k, lambda, phi, log = FALSE)
 }
 
 # Probability KumPar
+#' @rdname KumPar
 pkumpar<-function(q, beta, k, lambda, phi, lower.tail=TRUE, log.p = FALSE)
 {
   x<-q
@@ -20,6 +87,7 @@ pkumpar<-function(q, beta, k, lambda, phi, lower.tail=TRUE, log.p = FALSE)
 }
 
 # Quantile KumPar
+#' @rdname KumPar
 qkumpar<-function(p, beta, k, lambda, phi, lower.tail=TRUE, log.p = FALSE)
 {
   q <- beta/((1-(1-(1-p)^(1/phi))^(1/lambda))^(1/k))
@@ -28,6 +96,7 @@ qkumpar<-function(p, beta, k, lambda, phi, lower.tail=TRUE, log.p = FALSE)
 }
 
 # Hazard KumPar
+#' @rdname KumPar
 hkumpar<-function(q, beta, k, lambda, phi)
 {
   d <- (lambda*phi*k*beta^k)/(q^(k+1))*(1-(beta/q)^k)^(lambda-1)*(1-(1-(beta/q)^k)^lambda)^(phi-1)
@@ -38,6 +107,7 @@ hkumpar<-function(q, beta, k, lambda, phi)
 }
 
 # Cumulative Hazard KumPar
+#' @rdname KumPar
 #Hkumgg<-function(q, tau, alpha=1, k, lambda, phi)
 #{
 #  p<-pkumgg(q = q, tau = tau, alpha = alpha, k = k, lambda = lambda, phi = phi, lower.tail=FALSE)
@@ -45,6 +115,7 @@ hkumpar<-function(q, beta, k, lambda, phi)
 #}
 
 # Random KumPar
+#' @rdname KumPar
 rkumpar<-function(n, beta, k, lambda, phi, cens.prop=0)
 {
   if(cens.prop<0 | cens.prop>1){stop("cens.prop must be a numeric between 0 and 1.")}
